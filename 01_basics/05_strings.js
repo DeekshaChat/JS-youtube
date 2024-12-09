efc3a97fb20a692d195de3b17d26d4cbe761ae0d
DeekshaChat <51046017+DeekshaChat@users.noreply.github.com>
2024-12-09 09:34:48 +0000

    string and numbers

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+const userName = "Deeksha";
+
+const userTwo = new String("Deeksha");
+
+console.log(userName);
+console.log(userTwo);
+
+console.log(userName.charAt(4));
+console.log(userTwo.charAt(4));
+
+
+
