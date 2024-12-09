18724786c74cc8cce6075afb2e377fb9cc479c56
DeekshaChat <51046017+DeekshaChat@users.noreply.github.com>
2024-12-09 08:10:16 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,49 @@
+let score = 33;
+let valueInNumber = Number(score);
+
+// console.log(typeof score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//String
+let score1 = "33abc";
+let valueInNumber1 = Number(score1);
+
+// console.log(typeof score1);
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1);
+
+
+// String -other example
+let score2 = "Deeksha";
+let valueInNumber2 = Number(score2);
+
+// console.log(typeof score2);
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2);
+
+
+//boolean
+let score3 = true;
+let valueInNumber3 = Number(score3);
+
+// console.log(typeof score3);
+// console.log(typeof valueInNumber3);
+
+
+//null
+let score4 = null;
+let valueInNumber4 = Number(score4);
+
+// console.log(typeof score4);
+// console.log(typeof valueInNumber4);
+// console.log(valueInNumber4);
+
+//undefined
+let score5 = undefined;
+let valueInNumber5 = Number(score5);
+
+console.log(typeof score5);
+console.log(typeof valueInNumber5);
+console.log(valueInNumber5);
