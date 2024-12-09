546537c05a168967a1f54b2705d95132ec3dcc6b
DeekshaChat <51046017+DeekshaChat@users.noreply.github.com>
2024-12-09 07:50:51 +0000

    datatypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const userId = 1;
+let userName= "Deeksha";
+var userState="Rajasthan";
+let userCountry;
+
+console.table([userId, userName, userState, userCountry]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treats all JS code as newer version
+
+let userName= "Deeksha"; // string 
+let userAge = 28; // number
+
+/*
+number - 2 to the power 53
+bigint - more than number
+string
+boolean
+undefined
+null - standalone value
+symbol - unique
+
+object
+
+*/
+console.log(typeof null); //output: object
+console.log(typeof undefined); //output: undefined
